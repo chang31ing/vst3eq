7269ab3fde814146dc467d102a520081613bc963
chang31ing <azrieldreemur2006@gmail.com>
2025-05-19 20:46:04 +0300

    +grid lines, prerendered grid into bg

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -66,6 +66,8 @@ struct ResponseCurveComponent : juce::Component,
     void timerCallback() override;
 
     void paint(juce::Graphics& g) override;
+    
+    void resized() override;
 private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged{ false };
@@ -73,6 +75,12 @@ private:
     MonoChain monoChain;
 
     void updateChain();
+
+    juce::Image background;
+
+    juce::Rectangle<int> getRenderArea();
+
+    juce::Rectangle<int> getAnalysisArea();
 };
 
 //==============================================================================
